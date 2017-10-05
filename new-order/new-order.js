290d3441a50c6fb4d756a8a0cc4d2cf0023be1ff
NeXT <kingideayou@qq.com>
2017-10-05 21:18:57 +0800

    feature: 新订单页面

--- new-order/new-order.js
+++ new-order/new-order.js
@@ -1,3 +1,48 @@
+// 拉取技师列表
+// handlebars context
+var context = {
+  technicians: []
+};
+
+function setupData() {
+  // LeanCloud - 查询
+  // https://leancloud.cn/docs/leanstorage_guide-js.html#查询
+  var query = new AV.Query('Technician');
+  query.descending('createdAt');
+  query.find().then(function (technicians) {
+    technicians.forEach(function(technician) {
+      console.log(JSON.stringify(technician));
+      var name = technician.get('name');
+      var objectId = technician.get('objectId');
+      var releaseTime = technician.createdAt.getFullYear() + '/' + (technician.createdAt.getMonth() + 1) + '/' + technician.createdAt.getDate();
+      // var ownerUsername = order.get('owner').get('username');
+      // var productImage = order.get('image');
+      // var productImageUrl;
+      // if (productImage) {
+      //   productImageUrl = productImage.get('url');
+      // } else {
+      //   productImageUrl = './../storage.png'
+      // }
+      // handlebars context
+      context.technicians.push({
+        name,
+        objectId,
+        releaseTime
+      });
+    });
+
+    // use handlebars to update html
+    var source = $("#technicians-list").html();
+    var template = Handlebars.compile(source);
+    var html = template(context);
+    $('.technician-detail').html(html);
+
+  }).catch(function(error) {
+    alert(JSON.stringify(error));
+  });
+};
+
+//创建订单
 var Order = AV.Object.extend('Order');
 var OrderCustomerMap = AV.Object.extend('OrderCustomerMap');
 
@@ -10,6 +55,9 @@ function releaseNewOrder() {
   var customerFrom = $('#inputFrom').val();
   var assigned = $('#inputAssigned').val();
 
+  console.log('technicianId : ' + technicianId)
+
+  return;
   var query = new AV.Query('Technician');
   query.get(technicianId).then(function (technician) {
     if (technician == null) {
@@ -32,6 +80,7 @@ function releaseNewOrder() {
     order.set('paymentType', paymentType);
     order.set('customerFrom', customerFrom);
     order.set('assigned', assigned);
+    order.set('technician', technician);
     // order.set('customerType', description);
     // order.set('gender', description);
     // order.set('source', description);
@@ -56,6 +105,7 @@ function releaseNewOrder() {
 };
 
 $(function() {
+  setupData();
   if (isCurrentUser()) {
     $(".new-order").on('submit', function(e) {
       e.preventDefault();
